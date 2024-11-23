7cdc79ce0bf295bb7874cc79b385d4f968643e5f
Mathspy <mathspy257@gmail.com>
2024-11-23 12:20:51 -0500

    Fail on first error and run as sudo

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+set -e
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -y
 
 rustup target add wasm32-unknown-unknown
 
-curl -sSfL https://turbo.computer/install.sh | sh
+sudo curl -sSfL https://turbo.computer/install.sh | sh
 
 turbo export
 
