7903e8c21e35b9bbdd31171abb6e62a36b9bad87
Mathspy <mathspy257@gmail.com>
2024-11-23 12:32:20 -0500

    Don't use turbo's install script

--- build.sh
+++ build.sh
@@ -6,7 +6,11 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 rustup target add wasm32-unknown-unknown
 
-sudo curl -sSfL https://turbo.computer/install.sh | sh
+url="https://github.com/super-turbo-society/turbo-cli/releases/download/0.6.0/turbo-0.6.0-x86_64-unknown-linux-gnu.tar.gz"
+curl -L "$url" > turbo.tar.gz
+tar -xvf turbo.tar.gz --strip-components=1
+rm -rf turbo.tar.gz
+chmod +x turbo
 
-turbo export
+./turbo export
 
