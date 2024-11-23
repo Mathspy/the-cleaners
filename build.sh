d788cf14575d07b0f926c2dae58112012c671062
Mathspy <mathspy257@gmail.com>
2024-11-23 12:23:13 -0500

    Pass -y to rustup correctly

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 rustup target add wasm32-unknown-unknown
 
